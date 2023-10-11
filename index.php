114a6431ab7f236c1e936e02de039844fa433d94
Alfredo Gomez <alfretg.c@hotmail.com>
2023-10-11 03:28:25 -0500

    Modificacion de la estructura del archivo index.view.php y index.php

--- index.php
+++ index.php
@@ -1,49 +1,45 @@
 <?php
 require_once 'conexion/conexion.php';
 
-$movimiento = '';
+// Función para obtener el último registro de movimiento de un salón específico
+function obtenerUltimoMovimiento($salonId) {
+    global $conexion;
+    $statement = $conexion->prepare("SELECT * FROM regisalones WHERE salones_id = :salonId ORDER BY id DESC LIMIT 1");
+    $statement->bindParam(':salonId', $salonId, PDO::PARAM_INT);
+    $statement->execute();
+    $resultado = $statement->fetch();
+    return $resultado;
+}
 
-//Consultar ultimo registro de movimiento 1
-$statement = $conexion->prepare("SELECT * FROM regisalones WHERE estado = 1 ORDER BY id DESC LIMIT 1");
-$statement->execute();
-$resultados = $statement->fetchAll();
+// Función para calcular la diferencia de días entre dos fechas
+function calcularDiferenciaDias($fecha1, $fecha2) {
+    $fecha1 = strtotime($fecha1);
+    $fecha2 = strtotime($fecha2);
+    $diferencia = $fecha1 - $fecha2;
+    return round($diferencia / (60 * 60 * 24));
+}
 
-//Calcular fecha actual menos la fecha de la ultima actualizacion
-$ultimo = $resultados[0]['fecha'];
+// Consultar último registro de movimiento en el salón 1
+$movimientoSalon1 = obtenerUltimoMovimiento(25);
+
+// Consultar último registro de movimiento en el salón 2
+$movimientoSalon2 = obtenerUltimoMovimiento(26);
+
+// Consultar último registro de movimiento en el salón 3
+$movimientoSalon3 = obtenerUltimoMovimiento(27);
+
+// Consultar último registro de movimiento en el salón 4
+$movimientoSalon4 = obtenerUltimoMovimiento(32);
+
+// Calcular la diferencia de días
+$ultimoMovimiento = $movimientoSalon1['fecha'];
 $fechaActual = date("Y-m-d");
-$fecha = strtotime($fechaActual);
-$fechaUltimo = strtotime($ultimo);
-$diferencia = $fecha - $fechaUltimo;
-$diaEstimado = $diferencia / (60 * 60 * 24);
-$diaEstimado = round($diaEstimado);
+$diaEstimado = calcularDiferenciaDias($fechaActual, $ultimoMovimiento);
 
-//Saber si el movimiento es 0 o 1 para asignar color
-$consultarMovimiento1 = $conexion->prepare("SELECT * FROM regisalones WHERE salones_id = 25 ORDER BY id DESC LIMIT 1");
-$consultarMovimiento1->execute();
-$resultadoMovimiento1 = $consultarMovimiento1->fetchAll();
-$movimientoSalon1 = $resultadoMovimiento1[0]['estado'];
-
-$consultarMovimiento2 = $conexion->prepare("SELECT * FROM regisalones WHERE salones_id = 26 ORDER BY id DESC LIMIT 1");
-$consultarMovimiento2->execute();
-$resultadoMovimiento2 = $consultarMovimiento2->fetchAll();
-$movimientoSalon2 = $resultadoMovimiento2[0]['estado'];
-
-$consultarMovimiento3 = $conexion->prepare("SELECT * FROM regisalones WHERE salones_id = 27 ORDER BY id DESC LIMIT 1");
-$consultarMovimiento3->execute();
-$resultadoMovimiento3 = $consultarMovimiento3->fetchAll();
-$movimientoSalon3 = $resultadoMovimiento3[0]['estado'];
-
-$consultarMovimiento4 = $conexion->prepare("SELECT * FROM regisalones WHERE salones_id = 32 ORDER BY id DESC LIMIT 1");
-$consultarMovimiento4->execute();
-$resultadoMovimiento4 = $consultarMovimiento4->fetchAll();
-$movimientoSalon4 = $resultadoMovimiento4[0]['estado'];
-
-//Saber si el el valor de humo es 0 o 1 para asignar color
+print_r($ultimoMovimiento);
 
 require_once 'views/index.view.php';
 
-//Recargar pagina cada 5 segundos
-header("Refresh:2; url=index.php");
-
-
+// Recargar la página cada 5 segundos
+header("Refresh:5; url=index.php");
 ?>
--- views/index.view.php
+++ views/index.view.php
@@ -21,10 +21,10 @@
         <div class="contenedor__card contenedor__bano2 success">
             <h3>Baños 2</h3>
         </div>
-        <a href="tabla25.php" class="contenedor__card contenedor__salon1 <?php echo ($movimientoSalon1 == 1) ? 'danger' : 'success'; ?>">Salon 1</a>
-        <a href="tabla26.php" class="contenedor__card contenedor__salon2 <?php echo ($movimientoSalon2 == 1) ? 'danger' : 'success'; ?>">Salon 2</a>
-        <a href="tabla27.php" class="contenedor__card contenedor__salon3 <?php echo ($movimientoSalon3 == 1) ? 'danger' : 'success'; ?>">Salon 3</a>
-        <a href="tabla32.php" class="contenedor__card contenedor__salon4 <?php echo ($movimientoSalon4 == 1) ? 'danger' : 'success'; ?>">Salon 4</a>
+        <a href="tabla25.php" class="contenedor__card contenedor__salon1 <?php echo ($movimientoSalon1['estado'] == 1) ? 'danger' : 'success'; ?>">Salon 1</a>
+        <a href="tabla26.php" class="contenedor__card contenedor__salon2 <?php echo ($movimientoSalon2['estado'] == 1) ? 'danger' : 'success'; ?>">Salon 2</a>
+        <a href="tabla27.php" class="contenedor__card contenedor__salon3 <?php echo ($movimientoSalon3['estado'] == 1) ? 'danger' : 'success'; ?>">Salon 3</a>
+        <a href="tabla32.php" class="contenedor__card contenedor__salon4 <?php echo ($movimientoSalon4['estado'] == 1) ? 'danger' : 'success'; ?>">Salon 4</a>
 
         <a href="pasillo1.php" class="contenedor__card contenedor__pasillo1 <?php echo ($movimiento == 1 || $humoPasillo == 1) ? 'danger' : 'success'; ?>">Pasillo 1</a>
         <a href="pasillo2.php" class="contenedor__card contenedor__pasillo2 <?php echo ($movimiento == 1 || $humoPasillo == 1) ? 'danger' : 'success'; ?>">Pasillo 2</a>
@@ -35,9 +35,9 @@
 
         <div class="contenedor__alerta">
             <hr>
-            <h1>Ultimo registro de movimiento: Salon 2 hace <?php echo $diaEstimado . ' dias'; ?> (<?php echo $resultados[0]['fecha']; ?>)</h1>
+            <h1>Ultimo registro de movimiento: Salon 2 hace <?php echo $diaEstimado . ' dias'; ?> (<?php echo $ultimoMovimiento; ?>)</h1>
             <hr>
-            <h1>Ultimo registro de humo: pasillo 2 hace <?php echo $diaEstimado . ' dias'; ?> (<?php echo $resultados[0]['fecha']; ?>)</h1>
+            <h1>Ultimo registro de humo: pasillo 2 hace <?php echo $diaEstimado . ' dias'; ?> (<?php echo $ultimoMovimiento; ?>)</h1>
             <hr>
         </div>
 
