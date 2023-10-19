a6cf00bd72419a745a403056889e708ebb0adf7a
Alfredo Gomez <alfretg.c@hotmail.com>
2023-10-19 06:29:27 -0500

    Se creo la visualizacion del error en los dos pasillos

---
+++ js/ajaxpasillo2.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    function cargarEstados() {
+        $.ajax({
+            url: 'php/get_infopasillo2.php', // Reemplaza 'ruta/al/tu_script_php.php' con la ruta correcta a tu script PHP
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                if (data) {
+                    // Extraer los estados del objeto JSON recibido
+                    var estadohumoPasillo = data[0].estadohumoPasillo;
+                    var estadomovimientopasillo = data[0].estadomovimientopasillo;
+
+                    // Actualizar las clases de acuerdo con los estados
+                    var elementoHumo = $('.contenedor__humo');
+                    var elementoMovimiento = $('.contenedor__movimiento');
+                    console.log('Estado de humo del pasillo:', estadohumoPasillo);
+                    console.log('Estado de movimiento del pasillo:', estadomovimientopasillo);
+
+                    if (parseInt(estadohumoPasillo) === 1) {
+                        elementoHumo.removeClass('success').addClass('danger');
+                    } else {
+                        elementoHumo.removeClass('danger').addClass('success');
+                    }
+
+                    if (parseInt(estadomovimientopasillo) === 1) {
+                        elementoMovimiento.removeClass('success').addClass('danger');
+                    } else {
+                        elementoMovimiento.removeClass('danger').addClass('success');
+                    }
+                } else {
+                    console.log('No se encontraron datos.');
+                }
+            },
+            error: function () {
+                console.log('Error al cargar los datos.');
+            }
+        });
+    }
+
+    // Llamar a la función para cargar los estados inicialmente
+    cargarEstados();
+
+    // Actualizar la información cada 2 segundos
+    setInterval(cargarEstados, 2000);
+});
---
+++ php/get_infopasillo1.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../conexion/conexion.php';
+
+//Saber si el movimiento es 0 o 1 para asignar color
+$consultarMovimiento = $conexion->prepare("SELECT * FROM registromovi WHERE salones_id = 26 ORDER BY id DESC LIMIT 1");
+$consultarMovimiento->execute();
+$resultadoMovimiento = $consultarMovimiento->fetchAll();
+
+//Saber si el sensor de humo esta en 0 o 1
+$consultarPasillo = $conexion->prepare("SELECT * FROM registrohumo ORDER BY id DESC LIMIT 1");
+$consultarPasillo->execute();
+$resultadoPasillo = $consultarPasillo->fetchAll();
+
+$humoPasillo = $resultadoPasillo[0]['estado'];
+$movimiento = $resultadoMovimiento[0]['estado'];
+
+// Crear un array para almacenar los resultados
+$datos = array();
+
+// Agregar cada fila a un array asociativo
+$datos[] = array(
+    'estadohumoPasillo' => $humoPasillo,
+    'estadomovimientopasillo' => $movimiento
+);
+
+// Devolver los datos en formato JSON
+header('Content-Type: application/json');
+echo json_encode($datos);
+
+?>
